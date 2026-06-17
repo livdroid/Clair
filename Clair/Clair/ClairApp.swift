d83c6ebf2ff1d0cf1b275ef6bbe5e4a63f4cd4eb
Olivia <oliviarodrigue@gmail.com>
2026-06-17 09:30:19 +0200

    Initialize project

---
+++ Clair/Clair/ClairApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClairApp.swift
+//  Clair
+//
+//  Created by olivia on 17/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clair/ClairTests/ClairTests.swift
@@ -0,0 +1,18 @@
+//
+//  ClairTests.swift
+//  ClairTests
+//
+//  Created by olivia on 17/06/2026.
+//
+
+import Testing
+
+struct ClairTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
